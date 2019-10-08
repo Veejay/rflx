b461471b9bfb259d1392643e6a931e60213ac766
Bertrand Chardon <bertrand.chardon@gmail.com>
2019-10-09 00:21:15 +0200

    added jsdoc

--- rflx.js
+++ rflx.js
@@ -1,24 +1,51 @@
 class rflx {
-  static isObjectLiteral(something) {
+  /**
+   * @param {*} something
+   * @returns {Boolean} - whether or not something is an object literal
+   */
+  static isObject(something) {
     return Object.is(Object.prototype.call(something), "[object Object]");
   }
+  /**
+   * @param {*} something
+   * @returns {Boolean} - whether or not something is an array
+   */
   static isArray(something) {
     return Array.isArray(something);
   }
+  /**
+   * @param {*} something
+   * @returns {Boolean} - whether or not something is a string
   static isString(something) {
     return Object.is(typeof something, "string") || something instanceof String;
   }
+  /**
+   * @param {*} something
+   * @returns {Boolean} - whether or not something is a number
+   */
   static isNumber(something) {
     return Object.is(typeof something, "number");
   }
+  /**
+   * @param {*} something
+   * @returns {Boolean} - whether or not something is a map
   static isMap(something) {
     return something instanceof Map;
   }
+  /**
+   * @param {*} something
+   * @returns {Boolean} - whether or not something is a set
+   */
   static isSet(something) {
     return something instanceof Set;
   }
+  /**
+   * @param {*} something
+   * @returns {Boolean} - whether or not something is a boolean
+   */
   static isBoolean(something) {
-    return Object.is(something, 'boolean')
+    return Object.is(something, "boolean");
   }
 }
 
+module.exports = rflx;
